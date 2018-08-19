de0eff955aa7a22fa736cbb2564efc5529e8410c
lincolnzhou <875199116@qq.com>
2018-08-19 23:39:30 +0800

    redis

--- backend/app/router.go
+++ backend/app/router.go
@@ -3,16 +3,20 @@ package app
 import (
 	"net/http"
 
+	log "github.com/golang/glog"
 	"github.com/labstack/echo"
 	"github.com/lincolnzhou/check-in/backend/app/redis"
 	"github.com/lincolnzhou/check-in/backend/conf"
 )
 
+// InitRouter init router
 func InitRouter() {
-	// 初始化HTTP服务
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		redis.Incr("hit:index")
+		err := redis.Incr("hit:index")
+		if err != nil {
+			log.Infof("router / redis inrc error: %s", err.Error())
+		}
 
 		return c.String(http.StatusOK, "check in system")
 	})
