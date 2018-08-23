e817d4bb27f5a8c58acf77e36ba03ae12938dfb6
lincolnzhou <875199116@qq.com>
2018-08-24 00:23:33 +0800

    webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,21 +1,23 @@
+const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin'); 
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 let pathsToClean = [
-  'dist',
-  'build',
 	"static",
-	"views",
 ]
+
+let cleanOptions = {
+	root: path.resolve(__dirname, "../backend")
+}
 
 module.exports = {
 	devtool: 'eval-source-map',
 	entry: __dirname + "/app/main.js",
 	output: {
-		path: __dirname + "/static",
-		filename: "bundle-[hash:8].js"
+		path: path.resolve(__dirname, "../backend/static"),
+		filename: "js/[chunkhash:8].js"
 	},
 	devServer: {
 		contentBase: ".", // 本地页面目录
@@ -61,12 +63,13 @@ module.exports = {
 	plugins: [
 		new webpack.BannerPlugin("版权所有，翻版必究"),
 		new HtmlWebpackPlugin({
-			filename: __dirname + "/static/index.html",
+			filename: path.resolve(__dirname, "../backend/static/index.html"),
 			template: __dirname + "/app/index.tmpl.html"
 		}),
+		new webpack.optimize.OccurrenceOrderPlugin(),
 		new ExtractTextPlugin({
-			filename: '[hash:8].css'
+			filename: 'css/[hash:8].css'
 		}),
-		new CleanWebpackPlugin(pathsToClean),
+		new CleanWebpackPlugin(pathsToClean, cleanOptions),
 	]
 };
